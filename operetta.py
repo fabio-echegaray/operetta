Several changes: - Fix a bug regarding resolution (Fiji reported a wrong value). - Add an untility for batch processing. - Function measure_into_dataframe now includes centrosomes. - Operetta package has a new class that iterates over the images ith samples only. - Script position.py has new graphs. A very interesting one is distance distribution over the cell cycle.

operetta.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import re
 
@@ -7,6 +8,22 @@ from skimage import color
 from skimage import transform
 from skimage import exposure
 from skimage import io
+import shapely.wkt
+from shapely.geometry.point import Point
+
+from gui.utils import canvas_to_pil
+from gui.explore import RenderImagesThread
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('hhlab')
+
+
+def ensure_dir(file_path):
+    file_path = os.path.abspath(file_path)
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    return file_path
 
 
 class Montage:
@@ -72,15 +89,15 @@ class Montage:
         tubulin = color.gray2rgb(tubulin)
         pericentrin = color.gray2rgb(pericentrin)
 
-        red_multiplier = [1, 0, 0]
-        green_multiplier = [0, 1, 0]
-        blue_multiplier = [0, 0, 1]
-        out = hoechst * blue_multiplier * 0.6 + tubulin * green_multiplier * 0.2 + pericentrin * red_multiplier * 0.2
+        alexa_488 = [.29, 1., 0]
+        alexa_594 = [1., .61, 0]
+        alexa_647 = [.83, .28, .28]
+        hoechst_33342 = [0, .57, 1.]
+        out = hoechst * hoechst_33342 + tubulin * alexa_488 + pericentrin * alexa_594 + edu * alexa_647
 
-        io.imsave('/Volumes/Kidbeat/data/'
-                  'centr-dist(u2os)__2018-11-27T18_08_10-Measurement 1/'
-                  'render/%d-%d-%d.jpg' % (fid, row, col),
-                  out)
+        basepath = os.path.dirname(self.dir)
+        path = os.path.abspath(os.path.join(basepath, 'render/%d-%d-%d.jpg' % (fid, row, col)))
+        io.imsave(path, out)
 
     def max_projection(self, row, col, f):
         group = self.files[(self.files['row'] == row) & (self.files['col'] == col) & (self.files['f'] == f)]
@@ -98,3 +115,70 @@ class Montage:
                 max = np.maximum(max, img)
             channels.append(max)
         return channels
+
+
+class Dataframe(Montage):
+    def __init__(self, samples_path, images_path):
+        self.samples = pd.read_pickle(samples_path)
+        if np.any([i not in self.samples for i in ['row', 'col', 'fid']]):
+            is_row, is_col, is_fid = [i not in self.samples for i in ['row', 'col', 'fid']]
+            raise Exception('key columns not in provided dataframe row=%s col=%s fid=%s' % (is_row, is_col, is_fid))
+        self.images_path = images_path
+        self._super_initialized = False
+
+    def stack_generator(self):
+        samples = self.samples.groupby(['row', 'col', 'fid']).size().reset_index()
+        l = len(samples)
+        for k, r in samples.iterrows():
+            print('stack generator: retrieving %d of %d - row=%d col=%d fid=%d' % (k, l, r['row'], r['col'], r['fid']))
+            yield r['row'], r['col'], r['fid']
+
+    def save_render(self, row, col, fid, max_width=50):
+        hoechst, tubulin, pericentrin, edu = self.max_projection(row, col, fid)
+
+        hoechst = exposure.equalize_hist(hoechst)
+        tubulin = exposure.equalize_hist(tubulin)
+        pericentrin = exposure.equalize_hist(pericentrin)
+        edu = exposure.equalize_hist(edu)
+
+        hoechst = color.gray2rgb(hoechst)
+        tubulin = color.gray2rgb(tubulin)
+        pericentrin = color.gray2rgb(pericentrin)
+        edu = color.gray2rgb(edu)
+
+        alexa_488 = [.29, 1., 0]
+        alexa_594 = [1., .61, 0]
+        alexa_647 = [.83, .28, .28]
+        hoechst_33342 = [0, .57, 1.]
+        out = hoechst * hoechst_33342 * 0.25 + \
+              tubulin * alexa_488 * 0.25 + \
+              pericentrin * alexa_594 * 0.25 + \
+              edu * alexa_647 * 0.25
+
+        smpls = self.samples[(self.samples['row'] == row) & (self.samples['col'] == col) & (self.samples['fid'] == fid)]
+
+        basepath = os.path.dirname(self.dir)
+        for id, dfi in smpls.groupby('id'):
+            nucleus = shapely.wkt.loads(dfi['nucleus'].values[0])
+            cell = shapely.wkt.loads(dfi['cell'].values[0])
+            c1=shapely.wkt.loads(dfi['c1'].values[0])
+            c2=shapely.wkt.loads(dfi['c2'].values[0])
+            c1 = Point((20, 25))
+            c2 = Point((30, 35))
+            minx, miny, maxx, maxy = cell.bounds
+            ax, fig, canvas = RenderImagesThread.render(nucleus, cell, [c1, c2], width=200, height=200,
+                                                        xlim=[minx - 20, maxx + 20], ylim=[miny - 20, maxy + 20])
+
+            ax.imshow(out)
+
+            pil = canvas_to_pil(canvas)
+
+            path = os.path.abspath(os.path.join(basepath, 'render/r%d-c%d-f%d-i%d.jpg' % (row, col, fid, id)))
+            pil.save(ensure_dir(path))
+
+    def max_projection(self, row, col, fid):
+        if not self._super_initialized:
+            logger.info('Initializing parent of operetta.Dataframe...')
+            super().__init__(self.images_path)
+            self._super_initialized = True
+        return super().max_projection(row, col, fid)

position.py
@@ -1,152 +1,162 @@
 import logging
-import os
-import sys
 
+from sympy import lambdify
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from PyQt4.QtCore import *
-from PyQt4.QtGui import QApplication
 from matplotlib.ticker import EngFormatter
-from shapely.geometry import Point
-
-from gui.explore import ExplorationGui
-from gui.browse import BrowseGui
-import measurements as m
-import operetta as o
+import shapely.geometry
+from descartes import PolygonPatch
+from sympy import symbols
+from sympy.physics.mechanics import ReferenceFrame
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('hhlab')
 
 
-def measure(hoechst, pericentrin, edu, nuclei, cells, resolution):
-    out = list()
-    df = pd.DataFrame()
-    for nucleus in nuclei:
-        x0, y0, xf, yf = [int(u) for u in nucleus['boundary'].bounds]
+def s_phase_function(t, ref):
+    from sympy.codegen.cfunctions import log10
 
-        # search for closest cell boundary based on centroids
-        clls = list()
-        for cl in cells:
-            clls.append({'id': cl['id'],
-                         'boundary': cl['boundary'],
-                         'd': cl['boundary'].centroid.distance(nucleus['boundary'].centroid)})
-        clls = sorted(clls, key=lambda k: k['d'])
-
-        if m.is_valid_sample(hoechst, clls[0]['boundary'], nucleus['boundary'], nuclei):
-            pericentrin_crop = pericentrin[y0:yf, x0:xf]
-            logger.info('applying centrosome algorithm for nuclei %d' % nucleus['id'])
-            # self.pericentrin = exposure.equalize_adapthist(pcrop, clip_limit=0.03)
-            cntr = m.centrosomes(pericentrin_crop, max_sigma=resolution * 0.5)
-            cntr[:, 0] += x0
-            cntr[:, 1] += y0
-            cntrsmes = list()
-            for k, c in enumerate(cntr):
-                pt = Point(c[0], c[1])
-                pti = m.integral_over_surface(pericentrin, pt.buffer(resolution * 1))
-                cntrsmes.append({'id': k, 'pt': pt, 'i': pti})
-                cntrsmes = sorted(cntrsmes, key=lambda k: k['i'], reverse=True)
-
-            logger.debug('centrosomes {:s}'.format(str(cntrsmes)))
-
-            edu_int = m.integral_over_surface(edu, nucleus['boundary'])
-            dna_int = m.integral_over_surface(hoechst, nucleus['boundary'])
-
-            twocntr = len(cntrsmes) >= 2
-            c1 = cntrsmes[0] if len(cntrsmes) > 0 else None
-            c2 = cntrsmes[1] if twocntr else None
-
-            nucb = nucleus['boundary']
-            cllb = clls[0]['boundary']
-
-            lc = 2 if c2 is not None else 1
-            d = pd.DataFrame(data={'id': [nucleus['id']],
-                                   'dna_int': [dna_int],
-                                   'edu_int': [edu_int],
-                                   'centrosomes': [lc],
-                                   'c1_int': [c1['i'] if c1 is not None else np.nan],
-                                   'c2_int': [c2['i'] if c2 is not None else np.nan],
-                                   'c1_d_nuc_centr': [nucb.centroid.distance(c1['pt']) if c1 is not None else np.nan],
-                                   'c2_d_nuc_centr': [nucb.centroid.distance(c2['pt']) if twocntr else np.nan],
-                                   'c1_d_nuc_bound': [nucb.exterior.distance(c1['pt']) if c1 is not None else np.nan],
-                                   'c2_d_nuc_bound': [nucb.exterior.distance(c2['pt']) if twocntr else np.nan],
-                                   'c1_d_cell_centr': [cllb.centroid.distance(c1['pt']) if c1 is not None else np.nan],
-                                   'c2_d_cell_centr': [cllb.centroid.distance(c2['pt']) if twocntr else np.nan],
-                                   'c1_d_cell_bound': [cllb.exterior.distance(c1['pt']) if c1 is not None else np.nan],
-                                   'c2_d_cell_bound': [cllb.exterior.distance(c2['pt']) if twocntr else np.nan],
-                                   'c1_d_c2': [c1['pt'].distance(c2['pt']) if twocntr else np.nan],
-                                   'cell': cllb.wkb,
-                                   'nucleus': nucb.wkb
-                                   })
-            df = df.append(d, ignore_index=True, sort=False)
-
-            out.append({'id': nucleus['id'], 'cell': cllb, 'nucleus': nucb,
-                        'centrosomes': [c1, c2], 'edu_int': edu_int, 'dna_int': dna_int})
-    return out, df
-
-
-def batch_process_operetta_folder(path):
-    operetta = o.Montage(path)
-    outdf = pd.DataFrame()
-    for row, col, fid in operetta.stack_generator():
-        logger.info('%d %d %d' % (row, col, fid))
-        #     operetta.save_render(row, col, fid,max_width=300)
-        hoechst, tubulin, pericentrin, edu = operetta.max_projection(row, col, fid)
-        r = 30  # [um]
-        resolution = 1550.3e-4
-        imgseg, props = m.nuclei_segmentation(hoechst, radius=r * resolution)
-        operetta.add_mesurement(row, col, fid, 'nuclei found', len(np.unique(imgseg)))
-
-        if len(props) > 0:
-            outdf = outdf.append(props)
-
-            # self.nuclei_features = m.nuclei_features(imgseg, area_thresh=(r * self.resolution) ** 2 * np.pi)
-            nuclei = m.nuclei_features(imgseg)
-            for i, n in enumerate(nuclei):
-                n['id'] = i
-
-            cells, _ = m.cell_boundary(tubulin, hoechst)
-
-            samples, df = measure(hoechst, pericentrin, edu, nuclei, cells, resolution)
-            df['fid'] = fid
-            df['row'] = row
-            df['col'] = col
-            outdf = outdf.append(df, ignore_index=True, sort=False)
-
-    pd.to_pickle(outdf, 'out/nuclei.pandas')
-    operetta.files.to_csv('out/operetta.csv')
-    return outdf
+    f = t * ref.x + (log10(t - 2.2) + 17) * ref.y
+    Tn = f.diff(t, ref).normalize().simplify()
+    Nn = Tn.diff(t, ref).normalize().simplify()
+    sq = f.to_matrix(ref).applyfunc(lambda x: x ** 2)
+    # length = sympy.sqrt(sq.row(0)+sq.row(1))
+    # return lambdify(t, f.to_matrix(N)[1]), lambdify(t, Tn.to_matrix(N)), lambdify(t, Nn.to_matrix(N))
+    return lambdify(t, f.to_matrix(ref)[1]), Tn, Nn
 
 
 if __name__ == '__main__':
-    b_path = '/Volumes/Kidbeat/data/centr-dist(u2os)__2018-11-27T18_08_10-Measurement 1/Images'
-    # df = batch_process_operetta_folder(b_path)
     df = pd.read_pickle('out/nuclei.pandas')
     print(df.groupby(['fid', 'row', 'col', 'id']).size())
     print(len(df.groupby(['fid', 'row', 'col', 'id']).size()))
 
-    # df = df[df['c1_d_nuc_centr'] > 80]
-    # df = df[df['c1_int'] > 2000]
-    # df['c1_int'] = np.log(df['c1_int'])
+    df["geometry"] = df.apply(lambda row: shapely.geometry.Point(row['dna_int'] / 1e6 / 6, np.log(row['edu_int'])),
+                              axis=1)
+    df["cluster"] = -1
+
     fig = plt.figure(figsize=(8, 8))
     ax = fig.gca()
-    # map = ax.scatter(df['dna_int'], df['edu_int'], c=df['c1_int'], alpha=1)
-    # cbar = fig.colorbar(map)
-    # cbar.set_label('distance [um]', rotation=270)
-    sns.scatterplot(x="dna_int", y="edu_int", hue="c1_d_nuc_bound", size="c1_int",
-                    alpha=.5, palette="PRGn", data=df, ax=ax)
-
     ax.set_title('distance of the first centrosome with respect to nuleus centroid through cell cycle')
     ax.set_xlabel('dna [AU]')
     ax.set_ylabel('edu [AU]')
     formatter = EngFormatter(unit='')
     ax.xaxis.set_major_formatter(formatter)
     ax.yaxis.set_major_formatter(formatter)
-    ax.semilogy()
-    ax.set_xlim([6e6, 6e7])
-    ax.set_ylim([3e5, 1e8])
+    # ax.semilogy()
+    # ax.set_xlim([6e6, 6e7])
+    # ax.set_ylim([3e5, 1e8])
+    ax.set_xlim([1, 8])
+    ax.set_ylim([12, 18.5])
+    ax.set_aspect('equal')
+    # phony_log_formatter(ax)
+
+    map = ax.scatter(df['dna_int'] / 1e6 / 6, np.log(df['edu_int']), c=df['c1_d_nuc_bound'], alpha=1)
+    # map = ax.scatter(df['dna_int'], df['edu_int'], c=df['c1_int'], alpha=1)
+    # cbar = fig.colorbar(map)
+    # cbar.set_label('distance [um]', rotation=270)
+    # sns.scatterplot(x="dna_int", y="edu_int", hue="c1_d_nuc_bound", size="c1_int",
+    #                 alpha=.5, palette="PRGn", data=df, ax=ax)
+
+    # G1 ellipse
+    circ = shapely.geometry.Point((2.2, 13.8)).buffer(0.9)
+    ell = shapely.affinity.scale(circ, 1, 0.6)
+    ellr = shapely.affinity.rotate(ell, 20)
+
+    ix = df['geometry'].apply(lambda g: g.within(ellr))
+    df.loc[ix, 'cluster'] = 1
+
+    patch = PolygonPatch(ellr, fc="#FF0000", ec="#999999", alpha=0.5, zorder=2)
+    ax.add_patch(patch)
+
+    # S phase rect 1
+    poly = shapely.geometry.Polygon([(1.5, 14.4), (3.0, 14.4), (3.0, 15.4), (1.5, 15.4)])
+    patch = PolygonPatch(poly, fc="#0000FF", ec="#999999", alpha=0.6, zorder=2)
+    ax.add_patch(patch)
+
+    ix = df['geometry'].apply(lambda g: g.within(poly))
+    df.loc[ix, 'cluster'] = 2
+
+    # S phase rect 2
+    poly = shapely.geometry.Polygon([(3.3, 16.1), (5.5, 16.6), (5.5, 15.1), (3.2, 14.8)])
+    patch = PolygonPatch(poly, fc="#0000FF", ec="#999999", alpha=0.6, zorder=2)
+    ax.add_patch(patch)
+
+    ix = df['geometry'].apply(lambda g: g.within(poly))
+    df.loc[ix, 'cluster'] = 19
+
+    # G2 ellipse
+    circ = shapely.geometry.Point((4.35, 14.4)).buffer(1)
+    ell = shapely.affinity.scale(circ, 1.4, 0.6)
+    ellr = shapely.affinity.rotate(ell, 15)
+
+    ix = df['geometry'].apply(lambda g: g.within(ellr))
+    df.loc[ix, 'cluster'] = 20
+
+    patch = PolygonPatch(ellr, fc="#FF0000", ec="#999999", alpha=0.5, zorder=2)
+    ax.add_patch(patch)
+
+    x = np.linspace(0.1, 5.5, num=100)
+    t = symbols('t', positive=True)
+    N = ReferenceFrame('N')
+    f, tn, nn = s_phase_function(t, N)
+
+    h = 0.8
+    interior = lambdify(t, (nn * h).to_matrix(N))
+    exterior = lambdify(t, (nn * -h).to_matrix(N))
+
+    [ox], [oy], _ = interior(x) + np.array([[x], [f(x)], [0]])
+    [ix], [iy], _ = exterior(x) + np.array([[x], [f(x)], [0]])
+    ax.plot(ox, oy, c='#000AFF')
+    ax.plot(x, f(x), c='#FF00FF', zorder=1)
+    ax.plot(ix, iy, c='#000AFF')
+
+    n = len(np.arange(2.2, 5.5, step=0.5))
+    current_palette = sns.color_palette('bright', n)
+    print(current_palette)
+    xti = None
+    for k, xt in enumerate(np.arange(2.2, 5.5, step=0.7)):
+        x_ii = np.where(x <= xt)[0].max() + 1
+        xx = x[x_ii]
+
+        xi, yi, _ = interior(xx) + np.array([[xx], [f(xx)], [0]])
+        xf, yf, _ = exterior(xx) + np.array([[xx], [f(xx)], [0]])
+        ax.scatter(xx, f(xx), s=10, c='k', zorder=10)
+        ax.plot([xi, xf], [yi, yf], c='r', zorder=10)
+
+        if xti is not None:
+            x_ix = np.where((xti <= x) & (x <= xt))[0]
+            xx = x[np.append(x_ix, x_ix.max() + 1)]
+            [xi], [yi], _ = interior(xx) + np.array([[xx], [f(xx)], [0]])
+            [xf], [yf], _ = exterior(xx) + np.array([[xx], [f(xx)], [0]])
+            pointList = list()
+            pointList.extend([shapely.geometry.Point(x, y) for x, y in zip(xi, yi)])
+            pointList.extend([shapely.geometry.Point(x, y) for x, y in zip(np.flip(xf), np.flip(yf))])
+
+            poly = shapely.geometry.Polygon([(p.x, p.y) for p in pointList])
+            patch = PolygonPatch(poly, fc=current_palette[k], ec="#999999", alpha=0.6, zorder=2)
+            ax.add_patch(patch)
+
+            ix = df['geometry'].apply(lambda g: g.within(poly))
+            df.loc[ix, 'cluster'] = k + 2
+
+        xti = xt
     fig.savefig('facs.pdf')
+
+    print(df.groupby('cluster').size())
+
+    # fig = plt.figure(figsize=(5, 5))
+    # ax = fig.gca()
+    # print(df.groupby('cluster').size())
+    # with sns.color_palette('bright'):
+    #     for ii, dfi in df[df['cluster'] > 0].groupby('cluster'):
+    #         sns.distplot(dfi['c1_d_nuc_bound'].dropna(), hist=False, rug=True)
+    # fig.savefig('centr-distribution.pdf')
+
+    g = sns.FacetGrid(df[df['cluster'] > 0], row="cluster", height=1.5, aspect=5)
+    g = g.map(sns.distplot, "c1_d_nuc_bound", rug=True)
+    g.savefig('centr-distribution.pdf')
 
     #
     #
@@ -172,7 +182,17 @@ if __name__ == '__main__':
 
     plt.show()
 
-    if True:
+    if False:
+        import os
+        import sys
+        from PyQt4.QtCore import *
+        from PyQt4.QtGui import QApplication
+        from gui.explore import ExplorationGui
+        from gui.browse import BrowseGui
+        import operetta as o
+
+        b_path = '/Volumes/Kidbeat/data/centr-dist(u2os)__2018-11-27T18_08_10-Measurement 1/Images'
+
         base_path = os.path.abspath('%s' % os.getcwd())
         logging.info('Qt version:' + QT_VERSION_STR)
         logging.info('PyQt version:' + PYQT_VERSION_STR)
@@ -183,7 +203,7 @@ if __name__ == '__main__':
         app = QApplication(sys.argv)
 
         egui = ExplorationGui()
-        operetta = o.Montage(b_path)
+        operetta = o.Dataframe('out/nuclei.pandas', b_path)
         bgui = BrowseGui(operetta=operetta, exploration_gui=egui)
         # from pycallgraph import PyCallGraph
         # from pycallgraph.output import GraphvizOutput