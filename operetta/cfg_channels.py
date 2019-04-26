Some bugs were corrected in the ConfiguredChannels class ('cell' not in dataframe and SNR computation). The Montage class now generates a sample image for having a visual reference of the dataset. Uses of the new ConfiguredChannels class were updated in batch.py and position.py.

operetta/cfg_channels.py
@@ -190,7 +190,7 @@ class ConfiguredChannels(Montage):
 
         for ix, smp in dfi.groupby('id'):
             nucleus = shapely.wkt.loads(smp['nucleus'].iloc[0])
-            cell = shapely.wkt.loads(smp['cell'].iloc[0]) if not smp['cell'].isna().iloc[0] else None
+            cell = shapely.wkt.loads(smp['cell'].iloc[0]) if 'cell' in smp and not smp['cell'].isna().iloc[0] else None
 
             c1 = shapely.wkt.loads(smp['c1'].iloc[0]) if not smp['c1'].isna().iloc[0] else None
             c2 = shapely.wkt.loads(smp['c2'].iloc[0]) if not smp['c2'].isna().iloc[0] else None
@@ -218,7 +218,7 @@ class ConfiguredChannels(Montage):
         axc = fig_closeup.gca()
         for ix, smp in dfi.groupby('id'):
             nucleus = shapely.wkt.loads(smp['nucleus'].values[0])
-            cell = shapely.wkt.loads(smp['cell'].iloc[0]) if not smp['cell'].isna().iloc[0] else None
+            cell = shapely.wkt.loads(smp['cell'].iloc[0]) if 'cell' in smp and not smp['cell'].isna().iloc[0] else None
 
             c1 = shapely.wkt.loads(smp['c1'].iloc[0]) if not smp['c1'].isna().iloc[0] else None
             c2 = shapely.wkt.loads(smp['c2'].iloc[0]) if not smp['c2'].isna().iloc[0] else None
@@ -245,7 +245,7 @@ class ConfiguredChannels(Montage):
 
     def measure(self, *args):
         if len(args) == 1 and isinstance(args[0], int):
-            _id = args[0]
+            _id = int(args[0])
             r = self.files_gr.ix[_id - 1]
             row, col, fid = r['row'], r['col'], r['fid']
             logger.debug('measuring id=%d row=%d col=%d fid=%d' % (_id, row, col, fid))
@@ -344,7 +344,7 @@ class ConfiguredChannels(Montage):
                     if valid_sample:
                         tubulin_int = m.integral_over_surface(cell_img, cell_bnd)
                         tub_density = tubulin_int / cell_bnd.area
-                        if tub_density < 250:
+                        if tub_density < 150:
                             logger.warning(
                                 "sample rejected after validation because it had a low tubulin density")
                             logger.debug('tubulin density in cell: %0.2f, intensity %0.2f, area %0.2f' % (
@@ -440,7 +440,6 @@ class ConfiguredChannels(Montage):
                         cntrsmes = sorted(cntrsmes, key=lambda ki: ki['i'], reverse=True)
 
                     logger.debug('found {:d} centrosomes'.format(len(cntrsmes)))
-                    if len(cntrsmes) == 0: continue
 
                     twocntr = len(cntrsmes) >= 2
                     c1 = cntrsmes[0] if len(cntrsmes) > 0 else None
@@ -474,8 +473,8 @@ class ConfiguredChannels(Montage):
                 logger.debug('computing std dev of background for SNR calculation')
                 std_pericentrin = np.std(_img[cells_mask])
                 # u_pericentrin = np.mean(_img[cells_mask])
-                df['snr_c1'] = df['c1_int'] / std_pericentrin
-                df['snr_c2'] = df['c2_int'] / std_pericentrin
+                df['snr_c1'] = df['c1_int'].apply(lambda i: i / std_pericentrin if not np.isnan(i) else np.nan)
+                df['snr_c2'] = df['c2_int'].apply(lambda i: i / std_pericentrin if not np.isnan(i) else np.nan)
 
         if len(df) > 0:
             df['fid'] = fid

operetta/operetta.py
@@ -39,8 +39,6 @@ class Montage:
             self.files = self.generate_images_structure(folder)
             op_csv = ensure_dir(os.path.join(folder, 'out', 'operetta.csv'))
             self.files.to_csv(op_csv, index=False)
-            fgr = self.files.groupby(['row', 'col', 'fid']).size().reset_index()
-            logger.info("%d image stacks available." % len(fgr))
         else:
             self.files = pd.read_csv(csv_path)
 
@@ -55,6 +53,7 @@ class Montage:
             self.files = self.files[self.files['col'] == col]
             logger.debug('columns: %s' % self.files['col'].unique())
         self.files_gr = self.files.groupby(['row', 'col', 'fid']).size().reset_index()
+        logger.info("%d image stacks available." % len(self.files_gr))
 
         self.um_per_pix = None
         self.pix_per_um = None
@@ -64,7 +63,10 @@ class Montage:
         if os.path.exists(self.ffc_path):
             self.flat_field_profile()
 
-        if not os.path.exists(self.images_path):
+        if os.path.exists(self.images_path):
+            # generate sample image
+            self._generate_sample_image()
+        else:
             raise ImagesFolderNotFound('Images folder is not in the structure.')
 
     @staticmethod
@@ -208,6 +210,35 @@ class Montage:
 
         logger.info("layout succesfully extracted from xml file.")
         return self._layout
+
+    def _generate_sample_image(self):
+        from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+        from matplotlib.figure import Figure
+        from gui.utils import canvas_to_pil
+
+        cfg_ch = self.files.groupby(['ch', 'ChannelName']).size().reset_index().drop(0, axis=1)
+        max_ch = cfg_ch['ch'].max()
+        images = self.max_projection(np.random.randint(len(self.files_gr)))
+
+        width, height = images[0].shape
+        w_um, h_um = [s * self.um_per_pix for s in images[0].shape]
+        fig = Figure((width * 4 / 150, width * 4 / 150), dpi=150)
+        canvas_g = FigureCanvas(fig)
+
+        for i in range(4):
+            images = self.max_projection(np.random.randint(len(self.files_gr)))
+            for im, ch, title in zip(images, cfg_ch['ch'], cfg_ch['ChannelName']):
+                sp=i * 4 + ch
+                ax = fig.add_subplot(max_ch, 4, sp)
+                ax.imshow(im, extent=[0, w_um, h_um, 0], cmap='gray')
+                ax.set_xlim([0, w_um])
+                ax.set_ylim([0, h_um])
+                ax.set_axis_off()
+                ax.set_title(title)
+
+        pil = canvas_to_pil(canvas_g)
+        fpath = os.path.abspath(os.path.join(self.base_path, 'out', 'sample.jpg'))
+        pil.save(ensure_dir(fpath))
 
     def flat_field_profile(self):
         for root, directories, filenames in os.walk(self.ffc_path):

position.py
@@ -24,7 +24,7 @@ pd.set_option('display.width', 320)
 def move_images(df, render_path, destination_folder):
     os.makedirs(destination_folder, exist_ok=True)
     for i, r in df.iterrows():
-        name, original_path = o.FourChannels.filename_of_render(r, render_path)
+        name, original_path = o.ConfiguredChannels.filename_of_render(r, render_path)
         destination_path = os.path.join(destination_folder, name)
         try:
             logger.info('moving %s to %s' % (name, destination_folder))
@@ -136,7 +136,7 @@ def gate(df, den):
 
 def apply_gate_to_folder(pd_path, out_path):
     df = pd.read_pickle(pd_path)
-    df = df[df.apply(o.FourChannels.is_valid_measured_row, axis=1)]
+    df = df[df.apply(is_valid_measured_row, axis=1)]
     # print(df.groupby(['fid', 'row', 'col', 'id']).size())
     # print(len(df.groupby(['fid', 'row', 'col', 'id']).size()))
 
@@ -204,6 +204,17 @@ def apply_gate_to_folder(pd_path, out_path):
     # g = sns.FacetGrid(dfg, row="cluster", row_order=rorder, col="variable", height=1.5, aspect=3)
     # g = g.map_dataframe(_distplot, "value", rug=True)
     # g.savefig('{:s}/distribution-across-cc.pdf'.format(out_path))
+
+
+def is_valid_measured_row(row):
+    if row['cell'] is None: return False
+    if row['centrosomes'] < 1: return False
+    if row['tubulin_dens'] < 150: return False
+    if np.isnan(row['c1_int']): return False
+    if row['c2_int'] / row['c1_int'] < 0.6: return False
+    if shapely.wkt.loads(row['nucleus']).area < 5 ** 2 * np.pi: return False
+
+    return True
 
 
 if __name__ == '__main__':