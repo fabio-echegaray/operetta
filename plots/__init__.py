1a3f6c155c7c381fced6591c69e0b652c03084e4
Fabio Echegaray Iturra <fabio.echegaray@gmail.com>
2019-10-08 09:48:22 +0100

    Update graphs for ring.

--- plots/__init__.py
+++ plots/__init__.py
@@ -1,1 +1,16 @@
-from ._ring import Ring
+import matplotlib
+from .utils import colors
+from ._ring import Ring, eval_into_array
+
+# Type 2/TrueType fonts.
+matplotlib.rcParams.update({'pdf.fonttype': 42})
+matplotlib.rcParams.update({'ps.fonttype': 42})
+
+matplotlib.rcParams.update({'font.family': 'sans-serif'})
+matplotlib.rcParams.update({'font.sans-serif': ['Arial']})
+
+matplotlib.rcParams.update({'axes.titlesize': 8})
+matplotlib.rcParams.update({'axes.labelsize': 7})
+matplotlib.rcParams.update({'xtick.labelsize': 7})
+matplotlib.rcParams.update({'ytick.labelsize': 7})
+matplotlib.rcParams.update({'legend.fontsize': 7})
